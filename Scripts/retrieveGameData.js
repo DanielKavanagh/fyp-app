372d106ef1c18e9e24ecdfdfd0e37da8e6c26997
danielkavanagh <danielkavanaghwex@gmail.com>
2016-02-02 18:39:22 +0000

    Update retrieve data script

--- Scripts/retrieveGameData.js
+++ Scripts/retrieveGameData.js
@@ -9,6 +9,7 @@
 var request = require('request');
 var async = require('async');
 var fs = require('fs');
+var path = require('path');
 var parseString = require('xml2js').parseString;
 
 function main() {
@@ -18,7 +19,8 @@ function main() {
     }
 
     async.waterfall([
-        async.apply(getEIDs, process.argv[2])
+        async.apply(getEIDs, process.argv[2]),
+        getGameData
     ], function (err, result) {
         if (err) {
             console.log('Error: ' + err);
@@ -29,46 +31,87 @@ function main() {
 }
 
 function getEIDs(season, getEIDCallback) {
-    var weekIndex = 1;
-    var eidArray = [];
+    var weekIndex = 0;
+    var seasonArray = [];
 
     /*Loop over each week in the season*/
     async.whilst(
-        function () { return weekIndex < 2; },
+        function () { return weekIndex < 4; },
         function (weekCallback) {
+            var eidArray = [];
+            weekIndex++;
+
             request('http://www.nfl.com/ajax/scorestrip?season='+ season +'&seasonType=REG&week=' + weekIndex, function (err, response, body) {
                     if(err) {
                         console.log('HTTP Error: ' + err);
                         return;
                     }
 
-                    response.resume();
-
                     /*Add game EIDs to eidArray*/
                     parseString(body, function(err, result) {
                         async.each(result.ss.gms[0].g, function (id, callback) {
+
                             eidArray.push(id.$.eid);
                             callback();
                         });
                     });
 
+                    var weekObj = {
+                        "week": weekIndex,
+                        "eids": eidArray
+                    };
 
+                    seasonArray.push(weekObj);
 
-                    weekIndex++;
+                    response.resume();
+
                     weekCallback(null, weekIndex);
                 });
         },
-        function (err, loopResult) {
+        function (err) {
             /* Whilst has completed at this point */
             if (err) {
                 console.log('Error: ' + err);
-            } else {
-                console.log(loopResult);
+                return;
             }
 
-            getEIDCallback(null, 'Done!');
+            getEIDCallback(null, seasonArray, season);
         }
     );
+}
+
+/*Get the game data for each eid*/
+function getGameData(seasonArray, season, getGameCallback) {
+    //console.log(seasonArray);
+    async.each(seasonArray, function(obj, outerCallback) {
+        console.log(obj.week);
+        async.eachLimit(obj.eids, 1, function(weekObj, innerCallback) {
+            request('http://www.nfl.com/liveupdate/game-center/'+ weekObj +'/'+ weekObj +'_gtd.json', function(err, resp, body) {
+                if(!err && resp.statusCode == 200) {
+                    var jsonObj = JSON.parse(body);
+                    fs.writeFile(path.join(__dirname, '/data/' + season + '/week_' + obj.week + '/' + Object.keys(jsonObj)[0]), body, {flags: 'wx'}, function(err) {
+                        if(err) {
+                            return console.log('Error: ' + err);
+                        }
+
+
+                    });
+                }
+
+                innerCallback();
+
+            });
+
+        }, function(err) {
+            if(err) {
+                console.log(err);
+            } else {
+                outerCallback();
+            }
+
+        });
+
+    });
 }
 
 main();
