775e6fa45d8050a5940664a133564ea65af3fe9e
Daniel Kavanagh <danielkavanaghwex@gmail.com>
2016-02-25 21:46:38 +0000

    Adding player/team models

--- models/player.js
+++ models/player.js
@@ -1,3 +1,50 @@
 /**
  * Created by Daniel on 21/02/2016.
  */
+
+'use strict';
+
+var mysql = require('mysql');
+
+var Player = function (data) {
+    this.player_id = data.player_id;
+    this.gsis = data.gsis;
+    this.team_id = data.team_id;
+    this.first_name = data.first_name;
+    this.last_name = data.last_name;
+    this.position = data.position;
+    this.dob = data.dob;
+    this.weight_lb = data.weight_lb;
+    this.height_cm = data.height_cm;
+    this.college = data.college;
+    this.years_exp = data.years_exp;
+    this.uniform_num = data.uniform_num;
+    this.status = data.status;
+    this.profile_url = data.profile_url;
+};
+
+var method = Player.prototype;
+
+method.getAttribute = function (attribute) {
+    return this[attribute];
+};
+
+method
+
+method.setAttribute = function (attribute, value) {
+    this[attribute] = value;
+};
+
+method.save = function (callback) {
+
+};
+
+method.delete = function (callback) {
+
+}
+
+method.findByID = function (id, callback) {
+
+};
+
+module.exports = Player;
--- models/team.js
+++ models/team.js
@@ -1,3 +1,37 @@
 /**
  * Created by Daniel on 21/02/2016.
  */
+
+'use strict';
+
+var mysql = require('mysql');
+
+var Team = function (data) {
+    this.team_abbr = data.abbr;
+    this.team_name = data.name;
+    this.team_city = data.city;
+    this.team_division = data.division;
+    this.team_conference = data.conference;
+};
+
+var method = Team.prototype;
+
+method.getAttribute = function (attribute) {
+    return this[attribute];
+};
+
+method.setAttribute = function (attribute, value) {
+    this[attribute] = value;
+};
+
+method.save = function (callback) {
+
+};
+
+method.delete = function (callback) {
+
+};
+
+method.findByID = function (id, callback) {
+
+};
