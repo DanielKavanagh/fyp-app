d8e279da8aefeb0eafe3486162f803fa6fb554e7
Daniel Kavanagh <danielkavanaghwex@gmail.com>
2016-03-23 15:37:57 +0000

    Added feature sets 3 & 4, and team logos

--- scripts/createFeatureCSV.js
+++ scripts/createFeatureCSV.js
@@ -15,7 +15,7 @@ var json2csv = require('json2csv');
 
 async.waterfall([
     retrieveGames,
-    extractFeatureData_2
+    extractFeatureData_4
 ], function (err, result) {
     if (err) {
         return console.log(err);
@@ -309,6 +309,363 @@ function extractFeatureData_2(games, connection, callback) {
 
             fs.writeFile('/home/vagrant/fyp/fyp-app/data/features/' +
                 'featureTraining_2.csv', csv, function (err) {
+                if (err) {
+                    return callback(err);
+                }
+
+                callback(null, 'Feature File Saved');
+            });
+        });
+    });
+}
+
+function extractFeatureData_3(games, connection, callback) {
+    var columnNames = ['htfcr', 'hotfcr', 'haypp', 'hoaypp', 'hse', 'hose', 'hfa',
+            'atfcr', 'aotfcr', 'aaypp', 'aoaypp', 'ase', 'aose', 'gw'],
+        gameFeatureArray = [];
+
+    async.each(games, function (game, callback) {
+        console.log('Extracting game: ' + game.game_id);
+        async.parallel({
+            htfcr: function (callback) {
+                var sql = 'CALL homeThirdAndFourthConversionRate(' +
+                        game.home_team_id + ', ' + game.game_id +
+                        ', 8, @returnValue); SELECT @returnValue;';
+
+                connection.query(sql, function (err, result) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    callback(null, result[1][0]['@returnValue']);
+                });
+            },
+            hotfcr: function (callback) {
+                var sql = 'CALL overallThirdAndFourthConversionRate(' +
+                    game.home_team_id + ', ' + game.game_id +
+                    ', 16, @returnValue); SELECT @returnValue;';
+
+                connection.query(sql, function (err, result) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    callback(null, result[1][0]['@returnValue']);
+                });
+            },
+            haypp: function (callback) {
+                var sql = 'CALL homeAverageYardsPerPlay(' +
+                    game.home_team_id + ', ' + game.game_id +
+                    ', 8, @returnValue); SELECT @returnValue;';
+
+                connection.query(sql, function (err, result) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    callback(null, result[1][0]['@returnValue']);
+                });
+            },
+            hoaypp: function (callback) {
+                var sql = 'CALL overallAverageYardsPerPlay(' +
+                    game.home_team_id + ', ' + game.game_id +
+                    ', 16, @returnValue); SELECT @returnValue;';
+
+                connection.query(sql, function (err, result) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    callback(null, result[1][0]['@returnValue']);
+                });
+            },
+            hse: function (callback) {
+                var sql = 'CALL homeScoringEfficiency(' +
+                    game.home_team_id + ', ' + game.game_id +
+                    ', 8, @returnValue); SELECT @returnValue;';
+
+                connection.query(sql, function (err, result) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    callback(null, result[1][0]['@returnValue']);
+                });
+            },
+            hose: function (callback) {
+                var sql = 'CALL overallScoringEfficiency(' +
+                    game.home_team_id + ', ' + game.game_id +
+                    ', 16, @returnValue); SELECT @returnValue;';
+
+                connection.query(sql, function (err, result) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    callback(null, result[1][0]['@returnValue']);
+                });
+            },
+            hfa: function (callback) {
+                var sql = 'CALL overallHomeFieldAdvantage(' +
+                    game.home_team_id + ', ' + game.game_id +
+                    ', @returnValue); SELECT @returnValue;';
+
+                connection.query(sql, function (err, result) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    callback(null, result[1][0]['@returnValue']);
+                });
+            },
+            atfcr: function (callback) {
+                var sql = 'CALL awayThirdAndFourthConversionRate(' +
+                    game.away_team_id + ', ' + game.game_id +
+                    ', 8, @returnValue); SELECT @returnValue;';
+
+                connection.query(sql, function (err, result) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    callback(null, result[1][0]['@returnValue']);
+                });
+            },
+            aotfcr: function (callback) {
+                var sql = 'CALL overallThirdAndFourthConversionRate(' +
+                    game.away_team_id + ', ' + game.game_id +
+                    ', 16, @returnValue); SELECT @returnValue;';
+
+                connection.query(sql, function (err, result) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    callback(null, result[1][0]['@returnValue']);
+                });
+            },
+            aaypp: function (callback) {
+                var sql = 'CALL awayAverageYardsPerPlay(' +
+                    game.away_team_id + ', ' + game.game_id +
+                    ', 8, @returnValue); SELECT @returnValue;';
+
+                connection.query(sql, function (err, result) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    callback(null, result[1][0]['@returnValue']);
+                });
+            },
+            aoaypp: function (callback) {
+                var sql = 'CALL overallAverageYardsPerPlay(' +
+                    game.away_team_id + ', ' + game.game_id +
+                    ', 16, @returnValue); SELECT @returnValue;';
+
+                connection.query(sql, function (err, result) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    callback(null, result[1][0]['@returnValue']);
+                });
+            },
+            ase: function (callback) {
+                var sql = 'CALL awayScoringEfficiency(' +
+                    game.away_team_id + ', ' + game.game_id +
+                    ', 8, @returnValue); SELECT @returnValue;';
+
+                connection.query(sql, function (err, result) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    callback(null, result[1][0]['@returnValue']);
+                });
+            },
+            aose: function (callback) {
+                var sql = 'CALL overallScoringEfficiency(' +
+                    game.away_team_id + ', ' + game.game_id +
+                    ', 16, @returnValue); SELECT @returnValue;';
+
+                connection.query(sql, function (err, result) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    callback(null, result[1][0]['@returnValue']);
+                });
+            },
+            gw: function (callback) {
+                if (game.home_score_final > game.away_score_final) {
+                    callback(null, 'h');
+                } else if (game.home_score_final < game.away_score_final) {
+                    callback(null, 'a');
+                } else {
+                    callback(null, 'd');
+                }
+            }
+        }, function (err, results) {
+            if (err) {
+                return callback(err);
+            }
+
+            console.log('Extracted Features for Game: ' + game.game_id);
+
+            if (results.gw !== 'd') {
+                gameFeatureArray.push(results);
+            }
+
+            console.log(results);
+
+            callback(null);
+
+        });
+    }, function (err) {
+        //Loop has completed
+        if (err) {
+            return callback(err);
+        }
+
+        //Export feature set as CSV
+        console.log('Extracted features, exporting to csv');
+
+        json2csv({
+            data: gameFeatureArray,
+            fields: columnNames
+        }, function (err, csv) {
+            if (err) {
+                return callback(err);
+            }
+
+            fs.writeFile('/home/vagrant/fyp/fyp-app/data/features/' +
+                'featureTesting_3.csv', csv, function (err) {
+                if (err) {
+                    return callback(err);
+                }
+
+                callback(null, 'Feature File Saved');
+            });
+        });
+    });
+}
+
+function extractFeatureData_4(games, connection, callback) {
+    var columnNames = ['haypp', 'hse', 'hfa', 'aaypp', 'ase', 'gw'],
+        gameFeatureArray = [];
+
+    async.each(games, function (game, callback) {
+        console.log('Extracting game: ' + game.game_id);
+        async.parallel({
+            haypp: function (callback) {
+                var sql = 'CALL homeAverageYardsPerPlay(' +
+                    game.home_team_id + ', ' + game.game_id +
+                    ', 8, @returnValue); SELECT @returnValue;';
+
+                connection.query(sql, function (err, result) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    callback(null, result[1][0]['@returnValue']);
+                });
+            },
+            hse: function (callback) {
+                var sql = 'CALL homeScoringEfficiency(' +
+                    game.home_team_id + ', ' + game.game_id +
+                    ', 8, @returnValue); SELECT @returnValue;';
+
+                connection.query(sql, function (err, result) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    callback(null, result[1][0]['@returnValue']);
+                });
+            },
+            hfa: function (callback) {
+                var sql = 'CALL overallHomeFieldAdvantage(' +
+                    game.home_team_id + ', ' + game.game_id +
+                    ', @returnValue); SELECT @returnValue;';
+
+                connection.query(sql, function (err, result) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    callback(null, result[1][0]['@returnValue']);
+                });
+            },
+            aaypp: function (callback) {
+                var sql = 'CALL awayAverageYardsPerPlay(' +
+                    game.away_team_id + ', ' + game.game_id +
+                    ', 8, @returnValue); SELECT @returnValue;';
+
+                connection.query(sql, function (err, result) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    callback(null, result[1][0]['@returnValue']);
+                });
+            },
+            ase: function (callback) {
+                var sql = 'CALL awayScoringEfficiency(' +
+                    game.away_team_id + ', ' + game.game_id +
+                    ', 8, @returnValue); SELECT @returnValue;';
+
+                connection.query(sql, function (err, result) {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    callback(null, result[1][0]['@returnValue']);
+                });
+            },
+            gw: function (callback) {
+                if (game.home_score_final > game.away_score_final) {
+                    callback(null, 'h');
+                } else if (game.home_score_final < game.away_score_final) {
+                    callback(null, 'a');
+                } else {
+                    callback(null, 'd');
+                }
+            }
+        }, function (err, results) {
+            if (err) {
+                return callback(err);
+            }
+
+            console.log('Extracted Features for Game: ' + game.game_id);
+
+            if (results.gw !== 'd') {
+                gameFeatureArray.push(results);
+            }
+
+            console.log(results);
+
+            callback(null);
+
+        });
+    }, function (err) {
+        //Loop has completed
+        if (err) {
+            return callback(err);
+        }
+
+        //Export feature set as CSV
+        console.log('Extracted features, exporting to csv');
+
+        json2csv({
+            data: gameFeatureArray,
+            fields: columnNames
+        }, function (err, csv) {
+            if (err) {
+                return callback(err);
+            }
+
+            fs.writeFile('/home/vagrant/fyp/fyp-app/data/features/' +
+                'featureTraining_4.csv', csv, function (err) {
                 if (err) {
                     return callback(err);
                 }
