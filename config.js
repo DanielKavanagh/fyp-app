3e71f3035a448c4209d1e2e1206e2f839399a43b
Daniel Kavanagh <danielkavanaghwex@gmail.com>
2016-04-04 00:49:01 +0000

    Config changes

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = {
 
     db_prod: {
         host: 'fyp-rds.cz7l4bwnbihx.eu-west-1.rds.amazonaws.com',
-        user: 'danielk',
+        user: 'appuser',
         password: '*293962D1A8BEBEBE5F3ADF59F6530940F2CF2273',
         database: 'FYP_DB',
         multipleStatements: true
